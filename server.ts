159e5230a97f78d06f5f4e9005accad8dc1faa7a
Iam-Zarif <zarifraja19@gmail.com>
2025-12-04 13:43:08 +0600

    add

--- server.ts
+++ server.ts
@@ -1,47 +1,42 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import routes from "./routes";       
+import cors from "cors";
+import routes from "./routes";
 import { connectDB } from "./src/db";
-import cors from "cors"
 
 dotenv.config();
-
+  const port = process.env.PORT || 5000;
 const app = express();
+app.use(express.json());
+app.use(cookieParser());
+
 app.use(
   cors({
     origin: [
       "http://localhost:3000",
       "http://localhost:5173",
-      "https://cognitotrack.vercel.app",
+      "https://cognitotrack.vercel.app"
     ],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
-app.use(express.json());
-app.use(cookieParser());
-
 app.get("/", (req, res) => {
+  console.log("Incoming request to / route");
   res.status(200).json({
     success: true,
     message: "CognitoTrack API is working 🚀",
+    port:port
   });
 });
 
-
 app.use("/api", routes);
 
-connectDB()
-  .then(() => {
-    const port = process.env.PORT || 5000;
-    app.listen(port, () => {
-      console.log(`🚀 Server running on port ${port}`);
-    });
-  })
-  .catch((err) => {
-    console.error("❌ Failed to connect to MongoDB:", err);
-    process.exit(1);
-  });
+connectDB();
+
+if (process.env.NODE_ENV !== "production") {
+  app.listen(port, () => console.log(`Server running locally on port ${port}`));
+}
+
+export default app;
