9e566ae2bc6eeedfdd4b7d7520bf71c100bfaa0c
Iam-Zarif <zarifraja19@gmail.com>
2025-12-04 13:31:37 +0600

    add

--- server.ts
+++ server.ts
@@ -3,13 +3,34 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import routes from "./routes";       
 import { connectDB } from "./src/db";
+import cors from "cors"
 
 dotenv.config();
 
 const app = express();
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:5173",
+      "https://cognitotrack.vercel.app",
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "CognitoTrack API is working 🚀",
+  });
+});
+
 
 app.use("/api", routes);
 
